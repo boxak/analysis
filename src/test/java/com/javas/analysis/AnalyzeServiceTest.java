a05365652ef9baf224ffc480093a476ad6a8be64
boxak <boxak@naver.com>
2021-08-16 15:53:17 +0900

    2021-08-16 AnalyzeService 테스트 클래스 작성, properties 파일 수정

---
+++ src/test/java/com/javas/analysis/AnalyzeServiceTest.java
@@ -0,0 +1,116 @@
+package com.javas.analysis;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.javas.analysis.dto.News;
+import com.javas.analysis.repository.NewsRepository;
+import com.javas.analysis.repository.ResultRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import java.util.List;
+
+@Slf4j
+@DataMongoTest
+public class AnalyzeServiceTest {
+
+    @Autowired
+    NewsRepository newsRepository;
+
+    @Autowired
+    ResultRepository resultRepository;
+
+    @Test
+    void T1() throws Exception {
+        List<News> newsList = newsRepository.findAllByReadCheck(0);
+
+        News news = newsList.get(0);
+
+        log.info(news.toString());
+        log.info(extractKeywords(news));
+    }
+
+    private String extractKeywords(News news) throws Exception {
+        String title = news.getTitle();
+        String summary = news.getSummary();
+        String content = news.getContent();
+
+        title = nvl(title);
+        summary = nvl(summary);
+        content = nvl(content);
+
+        String text = title+"\n"+summary+"\n"+content;
+
+        JsonObject jsonObj = new JsonObject();
+        jsonObj.addProperty("text", text);
+        jsonObj.addProperty("analyzer", "korean_analyzer");
+
+        String query = jsonObj.toString();
+        log.info("jsonStr : {}",query);
+
+        String endPoint = "/nori_analyzer/_analyze";
+        Request request = new Request("POST", endPoint);
+        request.setJsonEntity(query);
+        RestClient client = restClientBuild();
+
+        Response response = client.performRequest(request);
+        String entityString = EntityUtils.toString(response.getEntity());
+
+        return getKeywords(entityString);
+    }
+
+    private String getKeywords(String entityString) {
+        JsonObject jsonObj = (JsonObject) JsonParser.parseString(entityString);
+        JsonArray jsonArray = (JsonArray) jsonObj.get("tokens");
+        StringBuilder sb = new StringBuilder();
+
+        for (JsonElement element : jsonArray) {
+            JsonObject tempObj = (JsonObject) element;
+            sb.append(String.valueOf(tempObj.get("token"))+",");
+        }
+
+        String str = sb.toString();
+
+        if (StringUtils.isNotEmpty(str)) {
+            str = str.substring(0, str.length()-1);
+        }
+
+        return str;
+    }
+
+    private RestClient restClientBuild() throws Exception {
+        RestClientBuilder builder = null;
+        try {
+            String server = "127.0.0.1";
+            HttpHost host = new HttpHost(server,9200,"http");
+            builder = RestClient.builder(host);
+            builder.setRequestConfigCallback(new RestClientBuilder.RequestConfigCallback() {
+                @Override
+                public RequestConfig.Builder customizeRequestConfig(RequestConfig.Builder builder) {
+                    return builder.setConnectTimeout(5000)
+                            .setSocketTimeout(5000)
+                            .setConnectionRequestTimeout(5000);
+                }
+            });
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return builder.build();
+    }
+
+    private String nvl(String str) {
+        return StringUtils.isEmpty(str) ? "" : str;
+    }
+}
