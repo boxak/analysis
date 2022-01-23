ebdca20fec9ff166e9c9aa198dc4070ca263f75b
boxak <boxak@naver.com>
2022-01-23 21:54:52 +0900

    2022-01-23

--- src/main/java/com/javas/analysis/service/JsonRPCService.java
+++ src/main/java/com/javas/analysis/service/JsonRPCService.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Service;
 @Service
 @JsonRpcService("/analyze_jsonrpc")
 public interface JsonRPCService {
-    ////
     String analyze(@JsonRpcParam(value = "mediaName") String mediaName);
 }
